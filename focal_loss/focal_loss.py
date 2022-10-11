b178639bc64ed65ba7e8428a8d6be2a9517f4abf
msalhab96 <mahmoud@salhab.work>
2022-10-11 17:02:07 +0300

    forward revamped, fixes #7 and fixes #6

--- focal_loss/focal_loss.py
+++ focal_loss/focal_loss.py
@@ -1,12 +1,18 @@
 import torch
 import torch.nn as nn
-import numpy as np
 from torch.nn.functional import one_hot
 from torch import Tensor
 
 
 class FocalLoss(nn.Module):
-    def __init__(self, alpha=1, gamma=2, reduction: str = 'mean'):
+    def __init__(
+            self,
+            alpha=1,
+            gamma=2,
+            reduction: str = 'mean',
+            ignore_index=-100,
+            eps=1e-16
+            ) -> None:
         super().__init__()
         if reduction not in ['mean', 'none', 'sum']:
             raise NotImplementedError(
@@ -15,6 +21,8 @@ class FocalLoss(nn.Module):
         self.reduction = reduction
         self.alpha = alpha
         self.gamma = gamma
+        self.ignore_index = ignore_index
+        self.eps = eps
 
     def _process_target(
             self, target: Tensor, num_classes: int
@@ -37,16 +45,21 @@ class FocalLoss(nn.Module):
         p = p * ~mask
         return p
 
-    def forward(self, x, target):
-        eps = np.finfo(float).eps
-        p_t = torch.where(target == 1, x, 1-x)
-        fl = - 1 * (1 - p_t) ** self.gamma * torch.log(p_t + eps)
-        fl = torch.where(target == 1, fl * self.alpha, fl * (1 - self.alpha))
-        return self._reduce(fl)
+    def forward(self, x: Tensor, target: Tensor) -> Tensor:
+        mask = target == self.ignore_index
+        x = self._process_preds(x)
+        num_classes = x.shape[-1]
+        target = self._process_target(target, num_classes)
+        pt = self._calc_pt(target, x, mask)
+        focal = 1 - pt
+        nll = -torch.log(self.eps + pt)
+        nll = nll.masked_fill(mask, 0)
+        loss = (focal ** self.gamma) * nll
+        return self._reduce(loss)
 
-    def _reduce(self, x):
+    def _reduce(self, x: Tensor, mask: Tensor) -> Tensor:
         if self.reduction == 'mean':
-            return x.mean()
+            return x.sum() / (~mask).sum()
         elif self.reduction == 'sum':
             return x.sum()
         else:
