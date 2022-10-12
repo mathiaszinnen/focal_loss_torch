8dcc6b39e1fa74179801a855a614fe6d76fb58b1
Mathias <mathias.zinnen@gmail.com>
2022-10-12 11:24:19 +0200

    Bump to 0.1.0

--- focal_loss/__init__.py
+++ focal_loss/__init__.py
@@ -1,3 +1,3 @@
 from .focal_loss import FocalLoss
 
-__version__ = '0.0.9'
+__version__ = '0.l.0'
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ README = (HERE / "README.md").read_text()
 setup(
     setup_requires=['wheel'],
     name="focal_loss_torch",
-    version="0.0.9",
+    version="0.1.0",
     description="Simple pytorch implementation of focal loss",
     long_description=README,
     long_description_content_type="text/markdown",
